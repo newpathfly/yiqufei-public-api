da8edc04a77d30064a0b88b02abac5af60aa1663
Tiger Wang <tigerwang@outlook.com>
2021-10-21 22:45:21 +0000

    WIP

--- src/main/java/com/yiqufei/api/model/Changes.java
+++ src/main/java/com/yiqufei/api/model/Changes.java
@@ -1,9 +1,15 @@
 package com.yiqufei.api.model;
 
+import java.math.BigDecimal;
+
 import com.yiqufei.api.model.enums.ChangesStatus;
 import com.yiqufei.api.model.enums.ChangesType;
+import com.yiqufei.api.model.enums.PassengerType;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -47,5 +53,65 @@ public class Changes {
     @NotNull
     ChangesStatus changeStatus;
 
-    // @todo - continue
+    /**
+     * ChangesFee
+     * 
+     * 1）when changesStatus=H, it must be assigned
+     * 
+     * 2）If changesStatus=T/F, this could not be assigned
+     */
+    @PositiveOrZero
+    BigDecimal changesFee;
+
+    /**
+     * When refundStatus=H, it must be assigned
+     */
+    @Size(min = 3, max = 3)
+    String currency;
+
+    /**
+     * PassengerType, 0=adult/1=child/2=infant
+     * 
+     * 1）For multi-passenger type request and verify, must be returned by the type
+     * of all passengers; e.g. adult+child's request, must return both of the
+     * allowance of exchange/refund.
+     */
+    @NotNull
+    PassengerType passengerType;
+
+    // @todo figure out what type is this
+    @NotBlank
+    String revNoshow;
+
+    /**
+     * When apply for change, how long before departure will be seen as NoShow?
+     * Unit: hours
+     * 
+     * If could not confirm for it, pls assigned as 0.
+     */
+    @NotNull
+    @PositiveOrZero
+    Integer revNoShowCondition;
+
+    /**
+     * Noshow change fee
+     * 
+     * 1)When revNoshow=H, it must be assigned
+     * 
+     * 2) Shows for users Noshow change
+     * 
+     * fee=changesFee+ revNoshowFee
+     */
+    @PositiveOrZero
+    BigDecimal revNoshowFee;
+
+    /**
+     * Chinese revert remark
+     */
+    String cnRevRemark;
+
+    /**
+     * English revert remark
+     */
+    String enRevRemark;
 }
--- src/main/java/com/yiqufei/api/model/Segment.java
+++ src/main/java/com/yiqufei/api/model/Segment.java
@@ -1,6 +1,7 @@
 package com.yiqufei.api.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,8 +20,77 @@ public class Segment {
      * Airlines IATA code, must be the same with the flightNumber
      */
     @NotBlank
-    @Size(min = 3, max = 3)
+    @Size(min = 2, max = 2)
     String carrier;
+
+    /**
+     * FlightNumber, e.g.: CA123
+     * 
+     * CZ6 flight number with extra zero, must be removed, e.g.CZ006, need to be
+     * return as CZ6
+     */
+    @NotBlank
+    @Size(min = 3)
+    String flightNumber;
+
+    /**
+     * Departure airport’s IATA code
+     */
+    @NotBlank
+    @Size(min = 3, max = 3)
+    String depAirport;
+
+    /**
+     * Departure terminal, in abbreviations, e.g. T1
+     */
+    String depTerminal;
+
+    /**
+     * Departure date and time, format: YYYYMMDDHHMM,
+     * 
+     * e.g. 201203100300 means 3 o’clock on March 10th, 2012
+     */
+    @NotBlank
+    @Size(min = 12, max = 12)
+    String depTime;
+
+    /**
+     * Arrival airport’s IATA code
+     */
+    @NotBlank
+    @Size(min = 3, max = 3)
+    String arrAirport;
+
+    /**
+     * Arrival terminal, in abbreviations, e.g. T1
+     */
+    String arrTerminal;
+
+    /**
+     * Arrival date and time, format: YYYYMMDDHHMM,
+     * 
+     * e.g. 201203101305 means 13:05 on March 10th, 2012 [only support Beijing time]
+     */
+    @NotBlank
+    @Size(min = 12, max = 12)
+    String arrTime;
+
+    /**
+     * Stop cities’s IATA code
+     */
+    String stopCities;
+
+    /**
+     * Stop airports’IATA code
+     */
+    String stopAirports;
+
+
+    /**
+     * Code share (true for code share/ false for none code share)
+     */
+    @NotNull
+    Boolean codeShare;
 
     // @todo - continue
 }
