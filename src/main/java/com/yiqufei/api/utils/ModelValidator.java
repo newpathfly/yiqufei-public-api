91151a7e4187b9af803b6057d09ffd023204ab64
Tiger Wang (王豫) <tigerwang@outlook.com>
2021-10-25 22:34:25 -0400

    More validations (#2)
    - updated fields according to the updated API document
    - minor updates to the validations per updated API document

--- src/main/java/com/yiqufei/api/utils/ModelValidator.java
+++ src/main/java/com/yiqufei/api/utils/ModelValidator.java
@@ -1,5 +1,9 @@
 package com.yiqufei.api.utils;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.Set;
 
 import com.yiqufei.api.model.Search;
@@ -11,6 +15,10 @@ import jakarta.validation.Validator;
 
 public class ModelValidator {
 
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter //
+            .ofPattern("uuuuMMdd") //
+            .withResolverStyle(ResolverStyle.STRICT);
+
     private final Validator _validator;
 
     public ModelValidator() {
@@ -18,12 +26,13 @@ public class ModelValidator {
     }
 
     public void validate(Search.Request request) {
-        if (TripType.RT.equals(request.getTripType())
-                && (null == request.getRetDate() || request.getRetDate().isEmpty()))
-            throw new IllegalArgumentException("retDate should not be null or empty since tripType is RT");
 
-        // @todo add validation for date format `uuuuMMdd`.
- 
+        validateDate(request.getFromDate());
+
+        if (TripType.RT.equals(request.getTripType())) {
+            validateDate(request.getRetDate());
+        }
+
         validateInternal(request);
     }
 
@@ -37,6 +46,24 @@ public class ModelValidator {
             if (null != violation)
                 throw new IllegalArgumentException(String.format("%s: %s::%s", violation.getMessage(),
                         violation.getLeafBean().getClass().getSimpleName(), violation.getPropertyPath().toString()));
+        }
+    }
+
+    private static void validateDate(String date) {
+        if (null == date) {
+            throw new IllegalArgumentException("date should not be null.");
+        }
+
+        LocalDate fromDate;
+        try {
+            fromDate = LocalDate.parse(date, DATE_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(String.format("date string is not in YYYYMMDD format: `%s`", date), e);
+        }
+
+        if (fromDate.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException(String.format("date `%s` should not be earlier than today `%s`", date,
+                    LocalDate.now().format(DATE_FORMATTER)));
         }
     }
 }
